d9239a55eff5e5fb72e1c3711143d0d3bfffd8ba
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-07-14 14:54:26 +0530

    add more options to anpr class

--- pyplatex/detection/yolo.py
+++ pyplatex/detection/yolo.py
@@ -1,5 +1,7 @@
+import os
 import asyncio
 import cv2
+from datetime import datetime
 from pyplatex.models import DETECTION_BASE_MODEL
 from ultralytics import YOLO
 import numpy as np
@@ -15,7 +17,7 @@ class ANPR:
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(None, Image.open, image_path)
 
-    async def detect(self, image_path, max_detections=1, confidence=0.6, save_image=False, PADDING=5):
+    async def detect(self, image_path, max_detections=1, confidence=0.6, save_image=False, padding=5, folder_name=None):
         # Load and preprocess the image
         image = await self.load_image(image_path)
         image = image.convert("RGB")
@@ -25,20 +27,44 @@ class ANPR:
         results = await asyncio.to_thread(self.model.predict, source=image, max_det=max_detections, conf=confidence)
 
         detected_plates = []
+        output_info = {
+            "detected": False,
+            "confidence": 0,
+            "saved_path": None
+        }
+
         for result in results:
             boxes = result.boxes.xyxy[0].numpy() if result.boxes else []
             conf = result.boxes.conf[0].numpy() if len(result.boxes.conf) > 0 else 0
-            detected_plates.append((boxes, conf))
+            
+            if boxes.size > 0:
+                detected_plates.append((boxes, conf))
+                output_info["detected"] = True
+                output_info["confidence"] = conf
 
         if save_image and detected_plates:
+            # Set folder name
+            if folder_name is None:
+                folder_name = "detected_plates"
+            # Create directory if it doesn't exist
+            if not os.path.exists(folder_name):
+                os.makedirs(folder_name)
+
+            # Create unique output path
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            output_path = os.path.join(folder_name, f"cropped_plate_{timestamp}.jpg")
+
+            # Add padding
             x1, y1, x2, y2 = boxes
-            x1 = max(0, int(x1) - PADDING)
-            y1 = max(0, int(y1) - PADDING)
-            x2 = min(image.shape[1], int(x2) + PADDING)
-            y2 = min(image.shape[0], int(y2) + PADDING)
+            x1 = max(0, int(x1) - padding)
+            y1 = max(0, int(y1) - padding)
+            x2 = min(image.shape[1], int(x2) + padding)
+            y2 = min(image.shape[0], int(y2) + padding)
             cropped_image = image[y1:y2, x1:x2]
 
-            output_path = "cropped_plate.jpg" 
             await asyncio.to_thread(cv2.imwrite, output_path, cropped_image)
 
-        return detected_plates
+            output_info["saved_path"] = output_path  # Save the file path
+
+        return output_info 
+
